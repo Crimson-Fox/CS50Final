f5bb180326a9f2edec6f99adf7dad1a5f120cbce
Stephen_Main <the.lone.spartan@gmail.com>
2023-06-17 02:14:19 +1000

    added new html and linked the nav updated content on the contact page with map and better form updated the labout layout added logic to app to serve the correct templates updated the index to have a overlay menu for form submission

--- CS50_final/website_rallis/app.py
+++ CS50_final/website_rallis/app.py
@@ -3,13 +3,13 @@ from flask import Flask, render_template, request, session, redirect, flash, jso
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods = ['POST','GET'])
 def index():
     '''this will be the index route'''
     print("Index")
     return render_template("/index.html")
 
-@app.route("/contactus")
+@app.route("/contactus", methods = ['POST','GET'])
 def contactus():
     '''this will be the index route'''
     print("contact Page")
@@ -18,11 +18,47 @@ def contactus():
 @app.route("/services")
 def services():
     '''this will be the services route'''
-    print("contact Page")
+    print("services Page")
     return render_template("/services.html")
 
 @app.route("/about")
 def about():
     '''this will be the services route'''
-    print("contact Page")
+    print("about Page")
     return render_template("/about.html")
+
+@app.route("/timber")
+def timber():
+    '''this will be the timber route'''
+    print("timber Page")
+    return render_template("/timber.html")
+
+@app.route("/suppliers")
+def suppliers():
+    '''this will be the suploers route'''
+    print("suppliers Page")
+    return render_template("/suppliers.html")
+
+@app.route("/hardware")
+def hardware():
+    '''this will be the suploers route'''
+    print("hardware Page")
+    return render_template("/hardware.html")
+
+@app.route("/sheets")
+def sheets():
+    '''this will be the suploers route'''
+    print("sheets Page")
+    return render_template("/sheets.html")
+
+@app.route("/doors")
+def doors():
+    '''this will be the suploers route'''
+    print("sheets Page")
+    return render_template("/doors.html")
+
+@app.route("/specialties")
+def specialties():
+    '''this will be the suploers route'''
+    print("specialtiesPage")
+    return render_template("/specialties.html")
